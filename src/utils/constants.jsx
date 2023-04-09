2e00ffc8509dff35030f7d1a822edcc20db0af78
DireWolf707 <rahulsood707@gmail.com>
2023-04-09 15:36:59 +0530

    added constants util file

---
+++ src/utils/constants.jsx
@@ -0,0 +1,33 @@
+import HomeIcon from "@mui/icons-material/Home"
+import InfoIcon from "@mui/icons-material/Info"
+import WorkIcon from '@mui/icons-material/Work';
+import CodeIcon from '@mui/icons-material/Code';
+import ContactPageIcon from '@mui/icons-material/ContactPage';
+
+export const navLinks = [
+  {
+    title: "HOME",
+    href: "#",
+    icon: <HomeIcon />,
+  },
+  {
+    title: "ABOUT",
+    href: "#",
+    icon: <InfoIcon />,
+  },
+  {
+    title: "WORK",
+    href: "#",
+    icon: <WorkIcon />,
+  },
+  {
+    title: "SKILLS",
+    href: "#",
+    icon: <CodeIcon />,
+  },
+  {
+    title: "CONTACT",
+    href: "#",
+    icon: <ContactPageIcon />,
+  },
+]
