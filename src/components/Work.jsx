a404b194202921d68b0015bc45cfd0972701f5d1
DireWolf707 <rahulsood707@gmail.com>
2023-04-09 20:43:51 +0530

    added work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,58 @@
-import React from "react"
-import { Stack } from "@mui/material"
+import React, { useState, useEffect } from "react"
+import { Stack, Typography, Button, Paper, Box } from "@mui/material"
+import { workTagsArray, workTags, projects } from "../utils/constants"
 
 const Work = () => {
-  return <Stack sx={{ height: "100vh" }}>Work</Stack>
+  const [selectedTag, setSelectedTag] = useState(workTags.all)
+  const [filteredProjects, setFilteredProjects] = useState([])
+
+  useEffect(() => {
+    if (selectedTag === workTags.all) return setFilteredProjects([...projects])
+
+    const _projects = [...projects].filter((project) => project.tags.includes(selectedTag))
+    setFilteredProjects(_projects)
+  }, [selectedTag])
+
+  return (
+    <Stack gap={4} p="38px">
+      {/* Heading */}
+      <Typography variant="h4" textAlign="center" fontWeight={600}>
+        <span style={{ color: "#FF793E" }}>Portfolio</span> Section
+      </Typography>
+      {/* Tag button group */}
+      <Stack flexWrap="wrap" flexDirection="row" justifyContent="center" gap={1.5}>
+        {workTagsArray.map((tag, idx) => (
+          <Button
+            key={idx}
+            onClick={() => setSelectedTag(tag)}
+            variant="contained"
+            size="small"
+            color={selectedTag === tag ? "error" : "my"}
+          >
+            {tag}
+          </Button>
+        ))}
+      </Stack>
+      {/* Projects */}
+      <Stack flexWrap="wrap" flexDirection="row" justifyContent="center" alignItems="start" gap={2}>
+        {filteredProjects.map((project, idx) => (
+          <Paper key={idx} elevation={10} sx={{ p: "16px" }}>
+            <Box component="img" src={project.image} height="280px" width="260px" sx={{ bgcolor: "#FF793E", borderRadius: "10px" }} />
+
+            <Stack gap={0.5} width="220px" mx="auto">
+              <Typography textAlign="center" fontWeight={600} color="#1d1160">
+                {project.title}
+              </Typography>
+
+              <Typography variant="subtitle2" textAlign="center" fontWeight={400} color="#1d1160">
+                {project.description}
+              </Typography>
+            </Stack>
+          </Paper>
+        ))}
+      </Stack>
+    </Stack>
+  )
 }
 
 export default Work
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,8 +1,8 @@
 import HomeIcon from "@mui/icons-material/Home"
 import InfoIcon from "@mui/icons-material/Info"
-import WorkIcon from '@mui/icons-material/Work';
-import CodeIcon from '@mui/icons-material/Code';
-import ContactPageIcon from '@mui/icons-material/ContactPage';
+import WorkIcon from "@mui/icons-material/Work"
+import CodeIcon from "@mui/icons-material/Code"
+import ContactPageIcon from "@mui/icons-material/ContactPage"
 
 export const navLinks = [
   {
@@ -29,5 +29,45 @@ export const navLinks = [
     title: "CONTACT",
     href: "#",
     icon: <ContactPageIcon />,
+  },
+]
+
+export const workTags = {
+  all: "all",
+  frontend: "frontend",
+  backend: "backend",
+  blockchain: "blockchain",
+  database: "database",
+  mern: "mern",
+  django: "django",
+  reactjs: "reactjs",
+}
+
+export const workTagsArray = Object.values(workTags)
+
+export const projects = [
+  {
+    title: "ABC",
+    image: "/about/wolf-howl.png",
+    description: "lorem lorem lorem lorem lorem lorem lorem lorem",
+    tags: [workTags.blockchain],
+  },
+  {
+    title: "ABC",
+    image: "/assets/wolf.svg",
+    description: "lorem lorem lorem lorem lorem",
+    tags: [workTags.frontend],
+  },
+  {
+    title: "ABC",
+    image: "/assets/wolf.svg",
+    description: "lorem lorem lorem lorem lorem",
+    tags: [workTags.backend],
+  },
+  {
+    title: "ABC",
+    image: "/assets/wolf.svg",
+    description: "lorem lorem lorem lorem lorem",
+    tags: [workTags.database],
   },
 ]
