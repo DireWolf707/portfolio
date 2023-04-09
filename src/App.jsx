7211d7ba7ca71377ca32741903a7656849f9da11
DireWolf707 <rahulsood707@gmail.com>
2023-04-09 19:08:49 +0530

    renamed header to about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Navbar, Header, About, Skills, Work, Sidebar } from "./components"
+import { Navbar, About, Skills, Work, Sidebar } from "./components"
 import { Stack } from "@mui/material"
 
 const App = () => {
@@ -9,15 +9,14 @@ const App = () => {
     <Stack
       sx={{
         background: "linear-gradient(135deg, rgba(0,0,0,1) 0%, rgba(0,0,149,1) 50%, rgba(0,129,255,1) 100%)",
-        overflow:"hidden"
+        overflow: "hidden",
       }}
     >
       <Sidebar open={open} setOpen={setOpen} />
       <Navbar setOpen={setOpen} />
-      <Header />
       <About />
-      <Skills />
       <Work />
+      <Skills />
     </Stack>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
 import Navbar from "./Navbar"
-import Header from "./Header"
 import About from "./About"
 import Work from "./Work"
 import Skills from "./Skills"
 import Sidebar from "./Sidebar"
 
-export { Navbar, Header, About, Skills, Work, Sidebar }
+export { Navbar, About, Skills, Work, Sidebar }
