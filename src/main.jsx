779dce929e5f6eef59fb8ef6ce2a07e6b7c30dd8
DireWolf707 <rahulsood707@gmail.com>
2023-04-09 20:36:17 +0530

    added custom mui color palette

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import { createTheme, ThemeProvider } from "@mui/material"
+import { red, blue } from "@mui/material/colors"
 import "./index.css"
 
-const theme = createTheme({})
+const theme = createTheme({
+  palette: {
+    my: {
+      dark: blue[50],
+      main: "white",
+      contrastText: red[400],
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider theme={theme}>
