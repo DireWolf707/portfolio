af325edf9c296fe78ab82d72ce8bee6cc94c50ce
DireWolf707 <rahulsood707@gmail.com>
2023-04-11 00:56:24 +0530

    added translation animation in contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,8 @@ import { Stack, Typography, Box, Link } from "@mui/material"
 import Heading from "./Heading"
 import Wrapper from "./Wrapper"
 import { contacts } from "../utils/constants"
+import { motion } from "framer-motion"
+import { translateLeft, translateRight } from "../utils/animations"
 
 const Contact = () => {
   return (
@@ -12,7 +14,15 @@ const Contact = () => {
       {/* Contact List */}
       <Stack gap={2} bgcolor="rgba(0,0,0,0.3)" m="auto" p="48px">
         {contacts.map((contact, idx) => (
-          <Stack key={idx} flexDirection="row" alignItems="center" gap={2}>
+          <Stack
+            component={motion.div}
+            whileInView={idx % 2 ? translateLeft : translateRight}
+            viewport={{ once: true }}
+            key={idx}
+            flexDirection="row"
+            alignItems="center"
+            gap={2}
+          >
             <Box component="img" src={contact.image} height="64px" width="64px" />
 
             {contact.link ? (
