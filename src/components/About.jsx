b649180fb2fc6b15607f5cda0d4e49ca7ab7380d
DireWolf707 <rahulsood707@gmail.com>
2023-04-11 01:28:41 +0530

    added whileInview animation in About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion"
 import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined"
 import Typed from "typed.js"
 import Wrapper from "./Wrapper"
-import { wolfAnimation, translateLeft, translateRight } from "../utils/animations"
+import { wolfAnimation, translateLeftVariant, translateRightVariant } from "../utils/animations"
 
 const About = () => {
   const textRef = useRef(null)
@@ -27,11 +27,31 @@ const About = () => {
       <Stack gap={3} sx={{ width: { xs: "320px", sm: "520px" } }}>
         {/* Welcome Bar */}
         <Stack gap={2} sx={{ flexDirection: { xs: "column", sm: "row" } }}>
-          <Typography component={motion.div} animate={translateRight} textAlign="center" variant="subtitle2" bgcolor="#FF793E" p="8px">
+          <Typography
+            component={motion.div}
+            variants={translateRightVariant}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            textAlign="center"
+            variant="subtitle2"
+            bgcolor="#FF793E"
+            p="8px"
+          >
             Welcome to my Portfolio
           </Typography>
 
-          <Button component={motion.div} animate={translateLeft} href="/assets/CV.pdf" target="_blank" variant="outlined" color="btn1">
+          <Button
+            component={motion.div}
+            variants={translateLeftVariant}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            href="/assets/CV.pdf"
+            target="_blank"
+            variant="outlined"
+            color="btn1"
+          >
             Download CV
           </Button>
         </Stack>
@@ -40,13 +60,31 @@ const About = () => {
           Hi! I'm Rahul <span ref={textRef} />
         </Typography>
         {/* Bio */}
-        <Typography component={motion.div} animate={translateRight} fontFamily="Righteous" fontSize="19px" color="#D2EFFF">
+        <Typography
+          component={motion.div}
+          variants={translateRightVariant}
+          initial="initial"
+          whileInView="animate"
+          viewport={{ once: true }}
+          fontFamily="Righteous"
+          fontSize="19px"
+          color="#D2EFFF"
+        >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, officiis quas? Quis, voluptatibus sed repellat consequuntur
           asperiores ea exercitationem ipsum magni rerum culpa, voluptates temporibus saepe deserunt! Atque, minima voluptatem.
         </Typography>
         {/* Connect Link */}
         <Link href="#contact" underline="none">
-          <Stack component={motion.div} animate={translateLeft} flexDirection="row" alignItems="center" gap={0.8}>
+          <Stack
+            component={motion.div}
+            variants={translateLeftVariant}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            flexDirection="row"
+            alignItems="center"
+            gap={0.8}
+          >
             <Typography fontWeight={600} color="#FF793E">
               Let's Connect
             </Typography>
